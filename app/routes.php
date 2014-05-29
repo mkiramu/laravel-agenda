f6a5c113f89a46ea63416ed0f6c7a3856fbf7764
Guillermo Dolores <momosolamente@gmail.com>
2014-05-29 11:18:45 -0500

    Rutas en laravel

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,46 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+Route::get('crear-usuario', function(){
+	return "URL crear-usuario";
+});
+
+
+Route::get('actualizar-usuario/{id_usuario}', function($id_usuario){
+	return "URL actualizar-usuario: ".$id_usuario;
+});
+
+
+Route::get('detalle-usuario/{id_usuario}', function($id_usuario){
+	return "URL detalle-usuario: ".$id_usuario;
+});
+
+
+Route::get('usuario/{id_usuario}/crear-contacto', function($id_usuario){
+	// return "URL crear-contacto: ".$id_usuario;
+	return Redirect::to('usuario/1/listar-contactos');
+});
+
+
+Route::get('usuario/{id_usuario}/actualizar-contacto/{id_contacto}', function($id_usuario, $id_contacto){
+	return "URL actualizar-contacto ".$id_usuario." ".$id_contacto;
+});
+
+
+Route::get('usuario/{id_usuario}/eliminar-contacto/{id_contacto}', function($id_usuario, $id_contacto){
+	return "URL eliminar-contacto ".$id_usuario." ".$id_contacto;
+});
+
+
+Route::get('usuario/{id_usuario}/listar-contactos/{id_contacto}', function($id_usuario, $id_contacto){
+	return "URL listar-contactos ".$id_usuario." ".$id_contacto;
+});
+
+
+Route::get('usuario/{id_usuario}/detalle-contacto/{$id_contacto}', function($id_usuario, $id_contacto){
+	return "URL detalle-contacto ".$id_usuario." ".$id_contacto;
+});
+
+
