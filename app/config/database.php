8ff6a366d5939a616e7bfcdd5bd689d0864e6969
Guillermo Dolores <momosolamente@gmail.com>
2014-05-29 10:30:12 -0500

    configuración básica reginal y de conexión

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'laravel_agenda',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'server',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
